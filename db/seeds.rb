acc69735486b1ab70e6d882cafb866597f37a91a
Mike Gehard <mike@samyamatech.com>
2009-09-18 08:35:19 -0600

    Cleaning up the repository as well as adding some new files to allow us to add seed and development data.

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# This file should not create bogus test data, only data needed to make the application run.
+# See the custom rake task in lib/tasks to populate test data.
+# This is new to 2.3.4.
+# Also see:
+# http://www.intridea.com/2008/4/19/seed-fu-simple-seed-data-for-rails?blog=company
+# http://railscasts.com/episodes/179-seed-data
+#
+# Examples:
+#   
+#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+#   Major.create(:name => 'Daley', :city => cities.first)
