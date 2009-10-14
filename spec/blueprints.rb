839987ebbe604a98f4fbff293d76507bd92e1c99
Mike Gehard <mike@samyamatech.com>
2009-10-14 08:33:20 -0600

    Created a blueprint hierarchy for the User object hierarchy.  You will need to make sure you are running the 1.0.4 version of Machinist (do this by running rake gems:install RAILS_ENV=test) to get this to work.

--- spec/blueprints.rb
+++ spec/blueprints.rb
@@ -26,54 +26,60 @@ Sham.define do
     the_array = (1..3).to_a
     the_array[Kernel.rand(the_array.length)]
   end
-  board_style {Faker::Lorem.words(1)}
   board_description {Faker::Lorem.words(10)}
   board_construction {Faker::Lorem.words(1)}
   location_street {Faker::Address.street_address()}
   location_locality {Faker::Address.us_state()}
-  location_region {""}
   location_postal_code {Faker::Address.zip_code()}
-  location_country {"USA"}
+  style_name {Faker::Lorem.words(1)}
 end
 
+# Start of blueprints for user hierarchy.
+# Subclass blueprints will first populate their attributes
+# And then look to their parent blueprint for the missing attributes
 Surfer.blueprint() do
   first_name
   last_name
-  email
-  password {"good_password"}
-  password_confirmation {password}
-  terms_of_service {true}
 end
 
 Shop.blueprint() do
   name {Sham.shop_name}
+end
+
+User.blueprint() do
   email
   password {"good_password"}
   password_confirmation {password}
   terms_of_service {true}
 end
+# End of user hierarchy blueprints
 
 Location.blueprint() do
+  user = User.make()
   street {Sham.location_street}
   locality {Sham.location_locality}
-  region {Sham.location_region}
+  region {"Region"}
   postal_code {Sham.location_postal_code}
-  country {Sham.location_country}
-  creator {Surfer.make()}
-  updater {Surfer.make()}
+  country {"USA"}
+  creator {user}
+  updater {user}
 end
 
 Board.blueprint() do
+  user = User.make()
   maker {Sham.board_maker}
   model {Sham.board_model}
   length {Sham.board_length}
   width {Sham.board_width}
   thickness {Sham.board_thickness}
-  style {Sham.board_style}
-  user {Surfer.make()}
+  style {Style.make()}
   description {Sham.board_description}
   location {Location.make()}
   construction {Sham.board_construction}
-  creator {Surfer.make()}
-  updater {Surfer.make()}
+  creator {user}
+  updater {user}
+end
+
+Style.blueprint() do
+  name {Sham.style_name}
 end
