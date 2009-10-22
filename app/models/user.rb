a6344c3a1e90e59ec1405f402cb616f086df7f72
Mike Gehard <mike@samyamatech.com>
2009-10-22 08:02:48 -0600

    Added back in password_confirmation presence check.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@ class User < ActiveRecord::Base
   validates_format_of       :email, :with => /.*@.*/
   validates_length_of       :password, :within => 4..40, :if => :password_required?
   validates_confirmation_of :password,                   :if => :password_required?
+  # This is needed to make sure that the validates_confirmation_of :password is run.
+  # Without this, if :password_confirmation is nil the confirmation check
+  # will not run. 
+  validates_presence_of     :password_confirmation
   validates_length_of       :email,    :within => 3..100
   validates_uniqueness_of   :email, :case_sensitive => false
 
