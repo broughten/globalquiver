db530afd89cb4366245fe5f0ece067d26c82a61f
Mike Gehard <mike@samyamatech.com>
2009-10-23 14:40:26 -0600

    New js file for the new users page.

---
+++ public/javascripts/users/new.js
@@ -0,0 +1,42 @@
+/* Do all of the things that you need to do 
+	once the page is ready to go. */
+$(document).ready(function(){
+   	initialize();
+ });
+
+function initialize(){
+	if ($('#is_shop')) {
+		// set up the initial state
+		toggleNames($('#is_shop').is(':checked'));
+		// register the event handlers.
+		$('#is_shop').click(function(){toggleNames($(this).is(':checked'))});
+	}
+}
+
+function toggleNames(isShop){
+    if(isShop){
+        hideSurferSpecificFields();
+        showShopSpecificFields();
+    }else{
+        showSurferSpecificFields();
+        hideShopSpecificFields();
+    }
+}
+
+function hideShopSpecificFields(){
+	$('#user_name').parent().hide();
+}
+
+function showShopSpecificFields(){
+	$('#user_name').parent().show();
+}
+
+function hideSurferSpecificFields(){
+	$('#user_first_name').parent().hide();
+    $('#user_last_name').parent().hide();
+}
+
+function showSurferSpecificFields(){
+	$('#user_first_name').parent().show();
+    $('#user_last_name').parent().show();
+}
