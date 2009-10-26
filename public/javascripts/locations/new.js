f6dbc386f5e02d2315bebc9426673d92437e4585
Mike Gehard <mike@samyamatech.com>
2009-10-26 13:15:52 -0600

    Revamping new locations screen to break up board creation flow a bit.

---
+++ public/javascripts/locations/new.js
@@ -0,0 +1,163 @@
+var geocoder = null;
+var DEFAULT_ZOOM = 10;
+
+/* Definition of the class to hold the address information */
+function Address(){
+	this.street = "";
+	this.city = "";
+	this.stats = "";
+	this.zipCode = "";
+	this.country = "";
+	this.accuracy = -1;
+	this.latLng = null;
+	this.addressString = "";
+}
+Address.prototype.isValid = function() {
+	return this.accuracy > 0;
+}
+Address.MIN_ACCURACY = 7;
+
+
+
+/* Do all of the things that you need to do 
+	once the page is ready to go. */
+$(document).ready(function(){
+   	toggleLocationSubmitButton(false);
+ });
+
+/* This should stay in the window.onload event.  Not sure why
+	but don't change it! */
+window.onload = addCodeToFunction(window.onload,function() {
+	GEvent.addListener(map, "click", getAddress);
+	geocoder = new GClientGeocoder();
+});
+
+
+function showAddress(addressString) {
+  if (geocoder) {
+    geocoder.getLocations(addressString, processGetLocationsResponse);
+  }
+	else{
+		alert("Unable to show address.");
+	}
+}
+
+function getAddress(overlay, latlng) {
+	if (geocoder && latlng != null) {
+		geocoder.getLocations(latlng, processGetLocationsResponse);
+	}
+}
+
+function processGetLocationsResponse(response) {
+  if (!response || response.Status.code != 200) {
+    alert("Sorry, we were unable to process that address. Please try again.");
+  } else {
+	var ourAddress = null;
+    var currentAccuracy = -1;
+    var newPlacemark = null;
+	var newAccuracy = -1;
+	var numberOfPlacemarks = response.Placemark.length;
+
+    //we do a for-loop here because if the address is generic enough it may bring back more than
+    //one result. which in the JSON return would be multiple Placemarks.
+	for (var i = 0; i < numberOfPlacemarks; i++) {
+		newPlacemark = response.Placemark[i];
+		newAccuracy = newPlacemark.AddressDetails.Accuracy;
+		if (newAccuracy > currentAccuracy) {
+			// we care about this placemark because it is more accurate 
+			// than the last one
+			ourAddress = getAddressFromPlacemark(newPlacemark);
+			currentAccuracy = newAccuracy;
+		}
+	}
+	if(ourAddress && ourAddress.isValid){
+		if(ourAddress.accuracy > Address.MIN_ACCURACY){
+			addAddressToMap(ourAddress);
+			populateFormFields(ourAddress);
+			toggleLocationSubmitButton(true);
+		}else{
+            addAddressToMap(ourAddress);
+			toggleLocationSubmitButton(false);
+			alert ("Address isn't exact enough.  Please try again.");
+		}
+	}else{
+		alert ("We couldn't find that address.  Please try again.");
+	}
+    
+  }
+}
+
+function addAddressToMap(currentAddress) {
+    map.clearOverlays();
+    var marker = new GMarker(currentAddress.latLng);
+    map.addOverlay(marker);
+    marker.openInfoWindowHtml('<b>Address: </b>' + currentAddress.addressString + '<br>');
+    map.setCenter(currentAddress.latLng,DEFAULT_ZOOM);
+}
+
+function populateFormFields(currentAddress){
+    $("#location_street").val(currentAddress.street);
+    $("#location_locality").val(currentAddress.city);
+    $("#location_region").val(currentAddress.state);
+    $("#location_postal_code").val(currentAddress.zipCode);
+    $("#location_country").val(currentAddress.country);
+    $("#location_accuracy").val(currentAddress.accuracy);
+}
+
+function toggleLocationSubmitButton(show){
+	if(show){
+		$("#location_submit").show();
+	}else{
+		$("#location_submit").hide();
+	}
+}
+
+function getAddressFromPlacemark(ourPlacemark){
+	var ourAddress = new Address();
+	ourAddress.accuracy = ourPlacemark.AddressDetails.Accuracy;
+	//some addresses have Locality and Thoroughfare under an admin area
+	//while others have them directly under country
+	
+	if (ourPlacemark.AddressDetails.Country.Locality != null) {
+		if (ourPlacemark.AddressDetails.Country.Locality.Thoroughfare != null) {
+ 			ourAddress.street = ourPlacemark.AddressDetails.Country.Locality.Thoroughfare.ThoroughfareName;
+		}
+		ourAddress.city = ourPlacemark.AddressDetails.Country.Locality.LocalityName;
+	} else {
+		//depending on the given address we may not have Locality and we may not have Thoroughfare
+		if (ourPlacemark.AddressDetails.Country.AdministrativeArea.Locality != null) {
+			ourAddress.city = ourPlacemark.AddressDetails.Country.AdministrativeArea.Locality.LocalityName;
+
+			//check for Thoroughfare
+			if (ourPlacemark.AddressDetails.Country.AdministrativeArea.Locality.Thoroughfare != null) {
+				ourAddress.street = ourPlacemark.AddressDetails.Country.AdministrativeArea.Locality.Thoroughfare.ThoroughfareName;
+            }
+
+            //check for Zip
+            if (ourPlacemark.AddressDetails.Country.AdministrativeArea.Locality.PostalCode) {
+				ourAddress.zipCode = ourPlacemark.AddressDetails.Country.AdministrativeArea.Locality.PostalCode.PostalCodeNumber;
+			}
+		} else {
+			//just found another address that has a sub administrative area.
+			//what a nightmare to parse this crap
+			if (ourPlacemark.AddressDetails.Country.AdministrativeArea.SubAdministrativeArea.Locality != null) {
+				ourAddress.city = ourPlacemark.AddressDetails.Country.AdministrativeArea.SubAdministrativeArea.Locality.LocalityName;
+			}
+			//check for Thoroughfare
+			if (ourPlacemark.AddressDetails.Country.AdministrativeArea.SubAdministrativeArea.Locality.Thoroughfare != null) {
+				ourAddress.street = ourPlacemark.AddressDetails.Country.AdministrativeArea.SubAdministrativeArea.Locality.Thoroughfare.ThoroughfareName;
+			}
+
+			//check for Zip
+			if (ourPlacemark.AddressDetails.Country.AdministrativeArea.SubAdministrativeArea.Locality.PostalCode) {
+				ourAddress.zipCode = ourPlacemark.AddressDetails.Country.AdministrativeArea.SubAdministrativeArea.Locality.PostalCode.PostalCodeNumber;
+			}
+
+		}
+		ourAddress.state = ourPlacemark.AddressDetails.Country.AdministrativeArea.AdministrativeAreaName;
+	}
+	ourAddress.country = ourPlacemark.AddressDetails.Country.CountryName;
+	ourAddress.latLng = new GLatLng(ourPlacemark.Point.coordinates[1], ourPlacemark.Point.coordinates[0]);
+	ourAddress.addressString = ourPlacemark.address;
+	return ourAddress;
+}
---
+++ spec/views/locations/new.html.erb_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe "/locations/new" do
+  
+  before(:each) do
+    assigns[:new_location] = Location.make_unsaved()
+    @existing_locations = mock()
+    assigns[:existing_locations] = @existing_locations
+    @existing_locations.stubs(:empty?).returns(true)
+    map = mock()
+    map.stubs(:div)
+    map.stubs(:to_html)
+    assigns[:map] = map
+  end
+
+  it "should have a form with text fields for the required fields" do
+    render "locations/new.html.erb"
+    response.should have_selector("form[method=post]", :action => locations_path) do |form|
+      form.should have_selector("input", :name=>"location[street]")
+      form.should have_selector("input", :name=>"location[locality]")
+      form.should have_selector("input", :name=>"location[region]")
+      form.should have_selector("input", :name=>"location[postal_code]")
+      form.should have_selector("input", :name=>"location[country]")
+      form.should have_selector("input[type=submit]") 
+    end
+  end
+    
+    it "should have a link to use an existing address when creating a new board if current_user has locations" 
+    
+    it "should not have a link to use an existing address when creating a new board if current_user does not have locations" 
+    
+  end
