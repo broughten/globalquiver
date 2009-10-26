9c8dc0950ccee4e393ac1e1701b3003a3876056e
Mike Gehard <mike@samyamatech.com>
2009-10-26 16:04:54 -0600

    Tweaks.

--- spec/spec_helpers/controllers/login_helpers.rb
+++ spec/spec_helpers/controllers/login_helpers.rb
@@ -1,7 +1,7 @@
 module ControllerHelpers
   def login_as_user
-    user = User.make()
-    controller.stubs(:current_user).returns(user)
+    @user = User.make()
+    controller.stubs(:current_user).returns(@user)
   end
  
 end
