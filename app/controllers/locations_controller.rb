2096469ba54c7cbf5ffea5970b29f388b39147d5
Mike Gehard <mike@samyamatech.com>
2009-10-26 13:16:22 -0600

    Edits to new location action to handle new board creation flow.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,90 +1,34 @@
 class LocationsController < ApplicationController
-  # GET /locations
-  # GET /locations.xml
-  def index
-    @locations = Location.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-    end
-  end
-
-  # GET /locations/1
-  # GET /locations/1.xml
-  def show
-    @location = Location.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-    end
-  end
-
+  before_filter :login_required
+  
   # GET /locations/new
-  # GET /locations/new.xml
   def new
-    @location = Location.new
-
+    @new_location = Location.new
+    @existing_locations = current_user.locations
+    @map = GMap.new("map")
+    
+    # Use the larger pan/zoom control but disable the map type
+    # selector
+    @map.control_init(:large_map => true,:map_type => false)
+    @map.center_zoom_init([25.165173,-158.203125],1)
+    
     respond_to do |format|
       format.html # new.html.erb
     end
   end
 
-  # GET /locations/1/edit
-  def edit
-    @location = Location.find(params[:id])
-  end
-
   # POST /locations
-  # POST /locations.xml
   def create
     @location = Location.new(params[:location])
 
     # Create a new map object, also defining the div ("map")
     # where the map will be rendered in the view
-    @map = GMap.new("map")
-    # Use the larger pan/zoom control but disable the map type
-    # selector
-    @map.control_init(:large_map => true,:map_type => false)
-    # Center the map on specific coordinates and focus in fairly
-    # closely
-
-    if (@location.latitude.nil?)
-      @map.center_zoom_init([25.165173,-158.203125], 1  )
-    else
-      @map.center_zoom_init([@location.latitude,@location.longitude], 12  )
-
-    end
+    
+    
 
     respond_to do |format|
       format.html # new.html.erb
     end
 
   end
-
-  # PUT /locations/1
-  # PUT /locations/1.xml
-  def update
-    @location = Location.find(params[:id])
-
-    respond_to do |format|
-      if @location.update_attributes(params[:location])
-        flash[:notice] = 'Location was successfully updated.'
-        format.html { redirect_to(@location) }
-      else
-        format.html { render :action => "edit" }
-      end
-    end
-  end
-
-  # DELETE /locations/1
-  # DELETE /locations/1.xml
-  def destroy
-    @location = Location.find(params[:id])
-    @location.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(locations_url) }
-    end
-  end
-
 end
