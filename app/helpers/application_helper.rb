d2eb2eb119a79898e2a074f11cafebe6c89364dd
Mike Gehard <mike@samyamatech.com>
2009-10-27 10:07:00 -0600

    Removed methods that aren't being used.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,15 +5,6 @@ module ApplicationHelper
     RAILS_ENV == 'production' ? 'https' : 'http'
   end
 
-  # Nested layouts, see: http://fora.pragprog.com/rails-recipes/write-your-own/post/144
-#  def inside_layout(layout, &block)
-#    @template.instance_variable_set("@content_for_layout", capture(&block))
-#
-#    layout = layout.to_s.include?("/") ? layout : "layouts/#{layout}" if layout
-#    buffer = eval("_erbout", block.binding)
-#    buffer.concat(@template.render(:file => layout))
-#  end
-
   def body_class
     if controller.respond_to?(:body_class)
       controller.send(:body_class)
@@ -22,33 +13,5 @@ module ApplicationHelper
     end
   end
 
-  def signup_or_login_page?
-    %w(session users).any? { |name| controller.class.name.downcase.include?(name) }
-  end
-
-  def static_page_keywords
-    PagesController::PAGE_KEYWORDS
-  end
-
-  def active_class_if_current_tab(keyword)
-    current_tab?(keyword) ? 'class="active"' : ''
-  end
-
-  def current_tab?(keyword)
-    current_page?(page_path(keyword)) || tab_conditions(keyword).any?(&:call)
-  end
-
-  def tab_conditions(keyword)
-    {
-      :home       => [ lambda { current_page?(root_path)             } ],
-      :about      => [ lambda { current_page?(page_path(:terms))     } ],
-      :my_account => [ lambda { ! %w(admin pages).any?{ |prefix| @controller.controller_path =~ /^#{prefix}/ }} ],
-      :admin      => [ lambda { @controller.controller_path =~ /^admin/ } ]
-    }[keyword.to_sym] || []
-  end
-
-  def more_than_one(collection)
-    collection.length > 0 ? 's' : ''
-  end
   
 end
