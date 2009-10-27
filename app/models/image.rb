2cd1025b61bb0b6e810554dad395ab86b212195a
Mike Gehard <mike@samyamatech.com>
2009-10-27 12:22:00 -0600

    Tweaked the image model.

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,8 +4,9 @@ class Image < ActiveRecord::Base
   #set up the Paperclip specific stuff
   # setting a style with the name of "original" will make sure that the actual image that
   # gets stored will not be too big. See http://railsforum.com/viewtopic.php?id=28064
-  # for more details.
-  has_attached_file :data, :styles => { :thumb => "100x100>",:original => "600x600" }        
+  # for more details.  We should set this style to the largest size that we want to allow on the 
+  # site
+  has_attached_file :data, :styles => { :thumb => "100x100>",:original => "600x600>" }        
   validates_attachment_content_type :data, :content_type => ['image/jpeg', 'image/png'], :message => "has to be in jpeg or png format"
   validates_attachment_size :data, :less_than => 2.megabytes
 end
