4882d9873c9c39d67befcc0d40866b4cb26941c4
Mike Gehard <mike@samyamatech.com>
2009-10-30 16:46:18 -0600

    New Schema.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091028173904) do
+ActiveRecord::Schema.define(:version => 20091030213323) do
+
+  create_table "board_searches", :force => true do |t|
+    t.integer  "geocode_id"
+    t.string   "board_type"
+    t.integer  "count"
+    t.integer  "1"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "boards", :force => true do |t|
     t.string   "maker"
