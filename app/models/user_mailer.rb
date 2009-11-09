a93c72c34a4dd77574fbaf4bd81ed7b2fc1655f6
Mike Gehard <mike@samyamatech.com>
2009-11-09 12:23:18 -0700

    Added new method to send out reservation updates to board renters.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -7,12 +7,24 @@ class UserMailer < ActionMailer::Base
   # and an array of added dates as the value.
   # The date_removals parameter will contain a hash with a board object as the key
   # and an array of removed dates as the value. 
-  def board_owner_board_date_change_notification(board_owner, date_additions, date_removals)
+  def board_owner_board_date_change_notification(board_owner, board_date_additions, board_date_removals)
     recipients(board_owner.email)
     from("GlobalQuiver <info@globalquiver.com>")
-    subject("Board Status Udate")
+    subject("Board Status Update")
     sent_on(Time.now)    
-    body({:date_additions => date_additions, :date_removals => date_removals})
+    body({:date_additions => board_date_additions, :date_removals => board_date_removals})
+  end
+  
+  # This method will send out an email to the board_renter that will
+  # contain information about upcoming reservations
+  # The board_rental_dates parameter will contain a hash with a board object as the key
+  # and an array of rental dates as the value.
+  def board_renter_upcoming_reservation_notification(board_renter, board_rental_dates)
+    recipients(board_renter.email)
+    from("GlobalQuiver <info@globalquiver.com>")
+    subject("Upcoming Board Reservation")
+    sent_on(Time.now)    
+    body({:board_rental_dates => board_rental_dates})
   end
 
 end
--- spec/models/user_mailer_spec.rb
+++ spec/models/user_mailer_spec.rb
@@ -1,13 +1,25 @@
 require 'spec_helper'
 
 describe UserMailer do
-  it "should send out an email for new reserved dates" do
+  it "should send out an email to board owners about date changes" do
     board = Board.make()
     dates = Array.new
     3.times{dates << UnavailableDate.make_unsaved.date}
 
     # Send the email, then test that it got queued
     email = UserMailer.deliver_board_owner_board_date_change_notification(board.creator, {board=>dates}, {board=>dates})
+    ActionMailer::Base.deliveries.should_not be_empty
+
+    # Test the body of the sent email contains what we expect it to
+  end
+  
+  it "should send out an email to board renters about upcoming reservations" do
+    board = Board.make()
+    dates = Array.new
+    3.times{dates << UnavailableDate.make_unsaved.date}
+
+    # Send the email, then test that it got queued
+    email = UserMailer.deliver_board_renter_upcoming_reservation_notification(board.creator, {board=>dates})
     ActionMailer::Base.deliveries.should_not be_empty
 
     # Test the body of the sent email contains what we expect it to
