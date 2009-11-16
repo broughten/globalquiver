cd5040f92da7089a14609a6aaec81f2fb93f4b8e
JC <mr.jonc@gmail.com>
2009-11-16 12:05:13 -0700

    fixed the overview view spec and added one test

--- spec/spec_helpers/views/user_helpers.rb
+++ spec/spec_helpers/views/user_helpers.rb
@@ -2,4 +2,8 @@ module ViewHelpers
   def build_surfer
     Surfer.make()
   end
+
+  def build_shop
+    Shop.make()
+  end
 end
--- spec/views/pages/overview.html.erb_spec.rb
+++ spec/views/pages/overview.html.erb_spec.rb
@@ -1,22 +1,40 @@
 require 'spec_helper'
 
 describe "pages/overview.html.erb" do
-  
-  describe "user has boards in their quiver" do
-    before(:each) do
-      @user = User.make
-      build_boards(10,@user)
-      assigns[:user] = @user
-      render "pages/overview.html.erb"
+
+  describe "surfer view" do
+    describe "surfer has boards in his quiver" do
+      before(:each) do
+        @user = build_surfer
+        template.controller.stubs(:current_user).returns(@user)
+        build_boards(10,@user)
+        assigns[:user] = @user
+        render "pages/overview.html.erb"
+      end
+
+      it "should have surfer info text" do
+        response.should include_text("Boards you've added to the Global Quiver")
+      end
+
+      it "should display a delete link for each board" do
+        @user.owned_boards.each {|board| response.should have_selector("a", :href=>"/boards/#{board.id}")}
+      end
     end
-  
-    it "should have a create board link" do
-      response.should have_selector("a", :href=> new_board_path)
+  end
+
+  describe "shop view" do
+    describe "shop has boards in its quiver" do
+      before(:each) do
+        @user = build_shop
+        template.controller.stubs(:current_user).returns(@user)
+        build_boards(10,@user)
+        assigns[:user] = @user
+        render "pages/overview.html.erb"
+      end
+
+      it "should have shop info text" do
+        response.should include_text("Below are the boards you are tracking via Global Quiver")
+      end
     end
-  
-    it "should display a delete link for each board" do
-      @user.owned_boards.each {|board| response.should have_selector("a", :href=>"/boards/#{board.id}")}
-    end
-    
   end
 end
