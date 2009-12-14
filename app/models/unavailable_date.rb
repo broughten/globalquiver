c79348bb55a881bae24e6e4465dcb4b4a09abf8d
Mike Gehard <mike@samyamatech.com>
2009-12-14 15:24:24 -0700

    Added table name to conditions to clear up ambiguity.

--- app/models/unavailable_date.rb
+++ app/models/unavailable_date.rb
@@ -11,12 +11,12 @@ class UnavailableDate < ActiveRecord::Base
 
   validates_presence_of :date
   
-  named_scope :created_since, lambda { |time| {:conditions => ["created_at > ?", time]} }
-  named_scope :deleted_since, lambda { |time| {:conditions => ["deleted_at > ?", time]} }
-  named_scope :inactive, :conditions => ["deleted_at IS NOT ?", nil]
-  named_scope :active, :conditions => ["deleted_at IS ?", nil]
-  named_scope :created_by, lambda { |user| {:conditions => ['creator_id = ?', (user.nil?)?-1:user.id]} }
-  named_scope :not_created_by, lambda { |user| {:conditions => ['creator_id != ?', (user.nil?)?-1:user.id]} }
+  named_scope :created_since, lambda { |time| {:conditions => ["unavailable_dates.created_at > ?", time]} }
+  named_scope :deleted_since, lambda { |time| {:conditions => ["unavailable_dates.deleted_at > ?", time]} }
+  named_scope :inactive, :conditions => ["unavailable_dates.deleted_at IS NOT ?", nil]
+  named_scope :active, :conditions => ["unavailable_dates.deleted_at IS ?", nil]
+  named_scope :created_by, lambda { |user| {:conditions => ['unavailable_dates.creator_id = ?', (user.nil?)?-1:user.id]} }
+  named_scope :not_created_by, lambda { |user| {:conditions => ['unavailable_dates.creator_id != ?', (user.nil?)?-1:user.id]} }
     
   before_validation_on_create :remove_soft_deleted_record
     
@@ -34,7 +34,7 @@ class UnavailableDate < ActiveRecord::Base
     # we need to check to see if there is a soft deleted record in the
     # db and delete it so the validation are OK
     # there should be only one
-    found_records = UnavailableDate.all(:conditions => ["parent_id = ? AND date = ? AND deleted_at IS NOT ?", self.parent_id, self.date, nil]) 
+    found_records = UnavailableDate.all(:conditions => ["parent_id = ? AND parent_type = ? AND date = ? AND deleted_at IS NOT ?", self.parent_id, self.parent_type, self.date, nil]) 
     found_records.each {|record| record.delete}
   end
   
