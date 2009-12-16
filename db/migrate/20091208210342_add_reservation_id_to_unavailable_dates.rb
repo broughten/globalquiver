d02e3079869e52e3d4e890ad58b73c9f500fa206
Mike Gehard <mike@samyamatech.com>
2009-12-16 10:13:12 -0700

    Not needed.

--- db/migrate/20091208210342_add_reservation_id_to_unavailable_dates.rb
+++
@@ -1,9 +0,0 @@
-class AddReservationIdToUnavailableDates < ActiveRecord::Migration
-  def self.up
-    add_column :unavailable_dates, :reservation_id, :integer
-  end
-
-  def self.down
-    remove_column :unavailable_dates, :reservation_id
-  end
-end
