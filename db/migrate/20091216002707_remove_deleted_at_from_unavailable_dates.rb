d87099baadcc42732a1f43fdadc2080a333adc59
JC <mr.jonc@gmail.com>
2009-12-15 22:41:03 -0700

    removed soft delete from unavailable_dates

--- db/migrate/20091216002707_remove_deleted_at_from_unavailable_dates.rb
+++ db/migrate/20091216002707_remove_deleted_at_from_unavailable_dates.rb
@@ -1,7 +1,10 @@
 class RemoveDeletedAtFromUnavailableDates < ActiveRecord::Migration
   def self.up
+    # this is no longer needed since we aren't doing soft delete on unavailable dates anymore
+    remove_column :unavailable_dates, :deleted_at
   end
 
   def self.down
+    add_column :unavailable_dates, :deleted_at, :timestamp, :default => nil
   end
 end
--- spec/models/unavailable_date_spec.rb
+++ spec/models/unavailable_date_spec.rb
@@ -74,37 +74,6 @@ describe UnavailableDate do
       result.length.should == 1      
     end
     
-    it "should find recently deleted unavailable dates based on a passed in date" do
-      unavailable_date1 = UnavailableDate.make(:deleted)
-      unavailable_date2 = UnavailableDate.make(:deleted)
-      result = UnavailableDate.deleted_since(2.days.ago)
-      result.should include(unavailable_date1)
-      result.length.should == 2
-      
-      unavailable_date1.deleted_at = 4.days.ago
-      unavailable_date1.save
-      result = UnavailableDate.deleted_since(2.days.ago)
-      result.should_not include(unavailable_date1)
-      result.length.should == 1      
-    end
-    
-    it "should contain inactive that only returns the inactive records" do
-      unavailable_date1 = UnavailableDate.make()
-      unavailable_date2 = UnavailableDate.make()
-      unavailable_date2.destroy()
-      results = UnavailableDate.inactive
-      results.should_not include(unavailable_date1)
-      results.should include(unavailable_date2)     
-    end
-    
-    it "should contain active that only returns the deleted records" do
-      unavailable_date1 = UnavailableDate.make()
-      unavailable_date2 = UnavailableDate.make()
-      unavailable_date2.destroy()
-      results = UnavailableDate.active
-      results.should include(unavailable_date1)
-      results.should_not include(unavailable_date2)     
-    end
     
     it "should be able to filter based on who created the unavailable date" do
       unavailable_date1 = UnavailableDate.make()
@@ -120,19 +89,6 @@ describe UnavailableDate do
       UnavailableDate.not_created_by(unavailable_date1.creator).should include(unavailable_date2)
     end
   end
-
-  it "should soft delete a record by setting deleted_at" do
-    date = UnavailableDate.make()
-    date.deleted_at.should be_nil
-    
-    date.destroy
-    date.deleted_at.should_not be_nil
-  end
-  
-  it "should allow new record to be added if a deleted one exists" do
-    deleted_unavailable_date = UnavailableDate.make(:deleted, :parent=>Board.make())
-    new_unavailable_date = UnavailableDate.make(:parent =>deleted_unavailable_date.parent, :date=>deleted_unavailable_date.date)
-    new_unavailable_date.should_not be_new_record
-  end
+ 
 
 end
