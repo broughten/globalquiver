ff721f4b6257e32d80fc1e1e7e3927d1672d4b04
Mike Gehard <mike@samyamatech.com>
2009-12-23 13:38:12 -0700

    Added in plugin to force http auth so we can keep the app locked down while in beta.

---
+++ vendor/plugins/miniauth/init.rb
@@ -0,0 +1,35 @@
+begin
+	miniauth = YAML.load(File.read("#{RAILS_ROOT}/config/miniauth.yml"))
+rescue Errno::ENOENT
+	puts "Could not read from config/miniauth.yml."
+	puts "Specify users and passwords in this file. Example:"
+	puts "john:\n  secret\ndave:\n  12345"
+	exit 1
+end
+
+if miniauth.class != Hash || miniauth.empty?
+	puts "Invalid auth spec in config/miniauth.yml."
+	puts "Specify users and passwords in a hash. Example:"
+	puts "john:\n  secret\ndave:\n  12345"
+	exit 2
+end
+
+ActionController::Base.send(:class_variable_set, '@@miniauth', miniauth)
+
+class ActionController::Base
+  before_filter :miniauth
+
+	def miniauth
+	  if (require_http_basic_auth)
+		  authenticate_or_request_with_http_basic do |username, password|
+			  @@miniauth.include?(username) && @@miniauth[username].to_s == password
+		  end
+	  end
+	end
+	
+	def require_http_basic_auth
+	  # don't require basic auth for local development
+	  request.domain.downcase != 'localhost'
+  end
+	
+end
