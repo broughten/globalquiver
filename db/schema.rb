c09174f48925aaba9a9cbd8f2b2427e294ecf239
Mike Gehard <mike@samyamatech.com>
2009-12-23 13:02:06 -0700

    Enable database sessions for the production environment.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091216054343) do
+ActiveRecord::Schema.define(:version => 20091223195650) do
 
   create_table "board_searches", :force => true do |t|
     t.integer  "style_id"
@@ -113,6 +113,16 @@ ActiveRecord::Schema.define(:version => 20091216054343) do
     t.datetime "updated_at"
     t.datetime "deleted_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "styles", :force => true do |t|
     t.datetime "created_at"
