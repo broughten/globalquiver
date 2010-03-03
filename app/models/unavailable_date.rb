e61eb994b1b403be67fb423c93eb0ce4b25e2d30
Mike Gehard <mike@samyamatech.com>
2010-03-02 19:25:09 -0700

    Removed the uniqueness of unavailable dates if the date is for a reservation of a generic board.

--- app/models/unavailable_date.rb
+++ app/models/unavailable_date.rb
@@ -6,8 +6,10 @@ class UnavailableDate < ActiveRecord::Base
   belongs_to :updater, :class_name => 'User'
 
 
-  validates_uniqueness_of :date, :scope => [:parent_id, :parent_type],
-    :message => 'This board was already unavailable on the selected date.'
+  validates_uniqueness_of :date, 
+    :scope => [:parent_id, :parent_type],
+    :message => 'This board was already unavailable on the selected date.',
+    :unless => :is_for_generic_board_reservation?
 
   validates_is_after :date, :on=>:create # makes sure the date is in the future but only when it is created
 
@@ -17,4 +19,7 @@ class UnavailableDate < ActiveRecord::Base
   named_scope :created_by, lambda { |user| {:conditions => ['unavailable_dates.creator_id = ?', (user.nil?)?-1:user.id]} }
   named_scope :not_created_by, lambda { |user| {:conditions => ['unavailable_dates.creator_id != ?', (user.nil?)?-1:user.id]} }
   
+  def is_for_generic_board_reservation?
+    self.parent.is_a?(Reservation) && self.parent.board.is_generic?
+  end
 end
